935f666c256afb0cbbc3dadfe5463a46da05ae21
blockhunter0007 <a23122160@gmail.com>
2026-07-31 20:56:58 +0200

    :3

---
+++ pull.bat
@@ -0,0 +1,4 @@
+git fetch --all
+git reset --hard origin/main
+echo "Pull complete!"
+timeout /t 2 /nobreak >nul
---
+++ pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git fetch --all
+git reset --hard origin/main
+echo "Pull complete!"
+sleep 2
