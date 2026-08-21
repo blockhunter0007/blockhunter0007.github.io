1debff73848e77ac26b52eabb49f7ef61283566a
blockhunter0007 <a23122160@gmail.com>
2026-08-22 03:23:30 +0400

    :3

--- test.py
+++ test.py
@@ -4,15 +4,26 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
-
 @app.route('/<path:path>')
 def serve(path):
+    out_put_file = ""
     if os.path.isfile(os.path.join(script_dir, path)):
-        return send_from_directory(script_dir, path)
+        out_put_file = path
     elif os.path.isdir(os.path.join(script_dir, path)):
-        return send_from_directory(script_dir, path + '/index.html')
+        out_put_file = path + "/index.html"
+    elif os.path.exists(path):
+        out_put_file = path
     else:
-        return "File not found", 404
+        #print(f"{script_dir}  {path}")
+        return "error"
+    with open(out_put_file, "rb") as file:
+        output = file.read()
+        try:
+            output = output.decode("utf-8")
+            #print(output)
+            return output.replace("https://blockhunter0007.github.io/", "http://127.0.0.1:5000/")
+        except:
+            return output
 
 @app.route('/')
 def index():
